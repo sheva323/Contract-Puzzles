56dff93f404584eef599e2b1e238d930a37286f7
sheva323 <sheva3232@gmail.com>
2023-03-24 08:50:10 -0500

    completed activity

--- test/game3Test.js
+++ test/game3Test.js
@@ -10,24 +10,30 @@ describe('Game3', function () {
     // you can get one of this accounts with ethers.provider.getSigner
     // and passing in the zero-based indexed of the signer you want:
     const signer = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    const signer3 = ethers.provider.getSigner(3);
 
     // you can get that signer's address via .getAddress()
     // this variable is NOT used for Contract 3, just here as an example
     const address = await signer.getAddress();
+    const address2 = await signer2.getAddress();
+    const address3 = await signer3.getAddress();
 
-    return { game, signer };
+    return { game, signer, signer2, signer3, address, address2, address3 };
   }
 
   it('should be a winner', async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer , signer2, signer3, address, address2, address3  } = await loadFixture(deployContractAndSetVariables);
 
     // you'll need to update the `balances` mapping to win this stage
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+    await game.connect(signer).buy({ value: '5' });
+    await game.connect(signer2).buy({ value: '10' });
+    await game.connect(signer3).buy({ value: '1' });
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(address, address2, address3);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,18 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
-
-    return { game };
+    const signer = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    const address = await signer.getAddress();
+    const address2 = await signer2.getAddress();
+    return { game, signer, signer2, address, address2 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer, signer2, address, address2  } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
-
-    await game.win();
+    await game.connect(signer2).write(address);
+    await game.connect(signer).win(address2);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
